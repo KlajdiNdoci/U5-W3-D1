7c12f31377e93da8286885c7e100a7e8fed4d0cd
Klajdi Ndoci <Klajdindoci@gmail.com>
2023-11-13 14:33:19 +0100

    added authenticateUser method

--- src/main/java/KlajdiNdoci/U5W2D5Project/services/AuthService.java
+++ src/main/java/KlajdiNdoci/U5W2D5Project/services/AuthService.java
@@ -1,6 +1,7 @@
 package KlajdiNdoci.U5W2D5Project.services;
 
 import KlajdiNdoci.U5W2D5Project.entities.User;
+import KlajdiNdoci.U5W2D5Project.exceptions.UnauthorizedException;
 import KlajdiNdoci.U5W2D5Project.payloads.UserLoginDTO;
 import KlajdiNdoci.U5W2D5Project.security.JWTTools;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,10 @@ public class AuthService {
     public String authenticateUser(UserLoginDTO body){
         User user = userService.findByEmail(body.email());
         if(body.password().equals(user.getPassword())){
+            return jwtTools.createToken(user);
 
+        }else {
+            throw new UnauthorizedException("Invalid credentials");
         }
     }
 }

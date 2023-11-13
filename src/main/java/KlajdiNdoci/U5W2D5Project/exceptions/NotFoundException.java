6e22cbdaa6ec91302333c97a5b49a7bfdc2cbe49
Klajdi Ndoci <Klajdindoci@gmail.com>
2023-11-13 14:07:49 +0100

    Initial commit

---
+++ src/main/java/KlajdiNdoci/U5W2D5Project/exceptions/NotFoundException.java
@@ -0,0 +1,8 @@
+package KlajdiNdoci.U5W2D5Project.exceptions;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(long id) {
+        super("Element with id " + id + " not found!");
+    }
+}

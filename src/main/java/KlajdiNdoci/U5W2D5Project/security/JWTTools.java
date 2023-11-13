b1d9ae5e47553d4ddcc56cc5770a2b0b128bb506
Klajdi Ndoci <Klajdindoci@gmail.com>
2023-11-13 15:51:13 +0100

    added config and filter

--- src/main/java/KlajdiNdoci/U5W2D5Project/security/JWTTools.java
+++ src/main/java/KlajdiNdoci/U5W2D5Project/security/JWTTools.java
@@ -1,6 +1,7 @@
 package KlajdiNdoci.U5W2D5Project.security;
 
 import KlajdiNdoci.U5W2D5Project.entities.User;
+import KlajdiNdoci.U5W2D5Project.exceptions.UnauthorizedException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,5 +18,19 @@ public class JWTTools {
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis()+ 1000 * 60 * 60 * 24 * 7))
                 .signWith(Keys.hmacShaKeyFor(secret.getBytes())).compact();
+    }
+
+    public void verifyToken(String token){
+        try{
+            Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
+                    .build().parse(token);
+        }catch (Exception e){
+            throw new UnauthorizedException("Unvalid token! Redo the login!");
+        }
+    }
+
+    public String extractIdFromToken(String token){
+        return Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
+                .build().parseClaimsJws(token).getBody().getSubject();
     }
 }
